ac3b2141b9f3fb51dc16d6fca4d74b903e9387db
Juan Pablo <pablo.silva0716@gmail.com>
2020-09-22 12:28:59 -0500

    DB_URI en el config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 // Dependencias
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 mongoose.set('useCreateIndex', true);
@@ -14,7 +15,10 @@ mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// Habilitar rutas publicas
+app.use( express.static(__dirname + '../public') );
 
 // Obtengo la informacion 
 app.use(require ('./routes/index'));
@@ -22,7 +26,7 @@ app.use(require ('./routes/index'));
 mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
   if(err) throw err;
 
-  console.log('DB ONLINE !!');
+  console.log('DB ONLINE ON', process.env.NODE_ENV);
 });
 
 // Inicializacion del server
